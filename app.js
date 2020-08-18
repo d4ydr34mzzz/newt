6f641ace364f8236ebec68a2dd1d532237fd68bb
d4y.dr34mzzz <d4y.dr34mzzz@gmail.com>
2020-08-18 11:19:00 -0700

    basic setup of Passport along with Google strategy

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const session = require('express-session');
 const mongoose = require('mongoose');
+const passport = require('passport');
+require('dotenv').config();
 const app = express();
+const authRouter = require('./routes/auth.js');
 const port = process.env.PORT || 3000;
+
+// Configure the strategies used by Passport
+require('./config/passport.js')(passport);
+
+// Passport middleware
+app.use(passport.initialize());
+// app.use(passport.session());
+
+// Mount the router module for auth on the /auth path in the main app
+app.use('/auth', authRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
