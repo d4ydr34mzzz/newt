86b7692cff5770911197fc126b80921055795aa3
d4y.dr34mzzz <d4y.dr34mzzz@gmail.com>
2020-08-18 17:12:12 -0700

    finished configuring the Google strategy for Passport
    still need to setup sessions and a session store

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,8 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 
+const User = mongoose.model('User');
+
 module.exports = function (passport) {
     // Configure the Google strategy
     passport.use(new GoogleStrategy(
@@ -11,8 +13,43 @@ module.exports = function (passport) {
             proxy: true
         },
         function (accessToken, refreshToken, profile, done) {
-            console.log(accessToken);
-            console.log(profile);
+            // Unlike a local strategy, we can create a user here because Google
+            // returns information about a user that we need that would otherwise require
+            // a registration form
+            User.findOne({ googleId: profile.id }).then((err, user) => {
+                if (err) {
+                    return done(err);
+                }
+
+                if (user) {
+                    return done(null, user);
+                } else {
+                    const newUser = {
+                        googleId: profile.id,
+                        email: profile.emails[0].value,
+                        firstName: profile.name.givenName,
+                        lastName: profile.name.familyName,
+                        picture: profile.photos[0].value
+                    }
+
+                    let user = new User(newUser)
+                    user.save().then((user) => {
+                        return done(null, user);
+                    }).catch((err) => {
+                        return done(err);
+                    });
+                }
+            });
         }
     ));
-}
+
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function (id, done) {
+        User.findById(id, function (err, user) {
+            done(err, user);
+        });
+    });
+};
