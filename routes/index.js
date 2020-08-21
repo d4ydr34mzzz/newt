42e8a6cc110022e13e9f58e73ba52480bae43d39
d4y.dr34mzzz <d4y.dr34mzzz@gmail.com>
2020-08-21 00:17:22 -0700

    added simple handlebar templates for dashboard and story related pages

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/dashboard', (req, res) => {
-    res.send('Welcome to the dashboard');
+    res.render('index/dashboard');
 });
 
 module.exports = router;
