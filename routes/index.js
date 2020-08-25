2108ae76b5572f6a8f0eb464b6ba2ee2d6a36619
d4y.dr34mzzz <d4y.dr34mzzz@gmail.com>
2020-08-24 21:16:01 -0700

    created preliminary profile page for users and started to implement the corresponding route handler
    "dashboard" should be changed to "profile" in the project

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 const { ensureAuthenticated, ensureGuest } = require('../helpers/auth.js');
+
+// Run the code in Story.js (no exports)
+require('../models/Story.js');
+
+// Retrieve the Story model
+const Story = mongoose.model('Story');
 
 router.get('/', ensureGuest, (req, res) => {
     res.render('index/welcome');
 });
 
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
-    res.render('index/dashboard');
+    Story.find({ user: req.user._id }).lean().then((stories) => {
+        res.render('index/dashboard', {
+            dashboardHeader: true,
+            stories: stories
+        });
+    }).catch((e) => {
+        // TODO: Need to redirect to an appropriate page and flash message
+        console.log(e);
+    });
 });
 
 module.exports = router;
