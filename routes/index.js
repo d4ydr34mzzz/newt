a0a8758cbe376cfa90ba85f072e3708dcea6ac52
d4y.dr34mzzz <d4y.dr34mzzz@gmail.com>
2020-08-27 23:13:51 -0700

    implemented the route handler for the /dashboard/:id path

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,12 @@ require('../models/Story.js');
 // Retrieve the Story model
 const Story = mongoose.model('Story');
 
+// Run the code in User.js (no exports)
+require('../models/User.js');
+
+// Retrieve the User model that was defined by running User.js
+const User = mongoose.model('User');
+
 router.get('/', ensureGuest, (req, res) => {
     res.render('index/welcome');
 });
@@ -23,6 +29,36 @@ router.get('/dashboard', ensureAuthenticated, (req, res) => {
         req.flash('error_message', 'There was an issue processing the request. Please try again later.');
         res.redirect('/stories');
     });
+});
+
+router.get('/dashboard/:id', (req, res) => {
+    // console.log(req.user.id)
+    if (req.user && (String(req.user._id) == req.params.id)) {
+        res.redirect('/dashboard');
+    } else {
+        User.findOne({ _id: req.params.id }).lean().then((user) => {
+            const userInfo = {
+                _id: user._id,
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                picture: user.picture,
+                fullName: user.firstName + ' ' + user.lastName
+            };
+            return userInfo;
+        }).then((userInfo) => {
+            Story.find({ user: userInfo._id, status: 'public' }).lean().then((stories) => {
+                res.render('index/dashboard', {
+                    dashboardHeader: true,
+                    stories: stories,
+                    secondaryUser: userInfo
+                });
+            });
+        }).catch((e) => {
+            req.flash('error_message', 'There was an issue processing the request. Please try again later.');
+            res.redirect('/stories');
+        });
+    }
 });
 
 module.exports = router;
